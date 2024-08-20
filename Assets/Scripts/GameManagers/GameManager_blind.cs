9b0cd1ac77fb913c334e8b18c8ab8ba93e6f178b
Marco Túlio Sandes Costa <mtsandes@yahoo.com>
2024-08-20 19:30:35 -0300

    Adding the mage character, organizing files and game objects, creating blind mode, improving menu

---
+++ Assets/Scripts/GameManagers/GameManager_blind.cs
@@ -0,0 +1,192 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class GameManagerBlind : MonoBehaviour {
+    public float Player1Health;
+    public float Player2Health;
+    public int RecordInt = 0;
+    public int StreakInt = 0;
+
+
+    public bool RestartGameBool = false;
+
+    public int SelectedCharacterP1;
+    public Character Player1Character;
+    public Character Player2Character;
+    public GameObject Player1Instance;
+    public GameObject Player2Instance;
+    public Animator Player1Animator;
+    public Animator Player2Animator;
+    [SerializeField] public CharacterDatabase CharacterDB;
+
+    public UIControllerBlind UIManager;
+    [SerializeField] public GameAudioController AudioController;
+
+    void Start() {
+        LoadCharacter();
+        UpdateCharacter(SelectedCharacterP1);
+
+        InstantiatePlayer();
+
+        Player1Health = Player1Character.Health;
+
+        SelectEnemy();
+
+        UIManager.UpdateHealth(Player1Health, Player2Health);
+
+        if (!PlayerPrefs.HasKey("RecordBlind")) {
+            PlayerPrefs.SetInt("RecordBlind", RecordInt);
+        }
+
+        else {
+            RecordInt = PlayerPrefs.GetInt("RecordBlind");
+        }
+
+        UIManager.UpdateRecord(StreakInt, RecordInt);
+
+        //  synthesizer.SpeakAsync("Welcome to the game. The current record is " + RecordInt);
+
+        AudioController.PlayCombatMusic();
+    }
+
+    public void Player1Attack() {
+        if (!RestartGameBool) {
+            Player2Health -= Player1Character.Damage;
+
+            Player1Animator.Play("Attack");
+            Player2Animator.Play("Hit");
+
+            AudioController.PlayHitSoundEffect();
+            // falar correct
+
+            UIManager.ShowPlayerHit();
+
+            if (Player2Health <= 0) {
+                Debug.Log("Player win");
+                Player1Health += 20;
+                SelectEnemy();
+
+                StreakInt++;
+
+                RecordInt = UIManager.UpdateRecord(StreakInt, RecordInt);
+            }
+
+            UIManager.UpdateHealth(Player1Health, Player2Health);
+        }
+    }
+
+    public void Player2Attack() {
+        if (!RestartGameBool) {
+            Player1Health -= Player2Character.Damage;
+
+            Player1Animator.Play("Hit");
+            Player2Animator.Play("Attack");
+
+            AudioController.PlayHitSoundEffect();
+            // falar incorrect
+
+            UIManager.ShowEnemyHit();
+
+            if (Player1Health <= 0) {
+                Player1Animator.Play("Die");
+
+                // falar you died com o record
+
+                // string deathMessage = "You Died. Press space to restart, Esc to quit.";
+
+                // if (StreakInt > RecordInt) {
+                //     deathMessage += " New record " + StreakInt;
+                // }
+
+                //  Speak(deathMessage);
+
+                Debug.Log("Enemy win");
+                ActivateRestartGameUI();
+            }
+
+            UIManager.UpdateHealth(Player1Health, Player2Health);
+        }
+    }
+
+    public void UpdateSequence(KeyCode[] sequence) {
+        string sequenceString = " ";
+
+        foreach (KeyCode key in sequence) {
+            sequenceString += key.ToString() + " ";
+        }
+
+        UIManager.UpdateSequence(sequenceString);
+    }
+
+    public void RestartGame() {
+        AudioController.PlayCombatMusic();
+
+        RestartGameBool = !RestartGameBool;
+
+        UIManager.RestartGame();
+
+        SelectEnemy();
+
+        Player1Health = Player1Character.Health;
+
+        UIManager.UpdateHealth(Player1Health, Player2Health);
+
+        Player1Animator.Play("Idle");
+
+        UIManager.UpdateRecord(StreakInt, RecordInt);
+    }
+
+    public void ActivateRestartGameUI() {
+        AudioController.PlayYouDiedMusic();
+
+        RestartGameBool = !RestartGameBool;
+
+        UIManager.ActivateRestartGameUI(StreakInt, RecordInt);
+
+        StreakInt = 0;
+    }
+   
+    public void SelectEnemy() {
+        if (Player2Instance != null) {
+            Destroy(Player2Instance);
+        }
+
+        Player2Character = CharacterDB.GetCharacter(GetRandomIndex(0, CharacterDB.CharacterCount - 1));
+        
+        InstantiateEnemy();
+
+        // Speak("Enemy defeated. Next enemy: " + Player2Character.Name + " with " + Player2Character.Health + " health.");
+
+        Player2Health = Player2Character.Health;
+    }
+
+    public static int GetRandomIndex(int min, int max) {
+        System.Random random = new System.Random();
+        return random.Next(min, max + 1);
+    }
+
+    private void LoadCharacter() {
+        SelectedCharacterP1 = PlayerPrefs.GetInt("SelectedCharacterP1");
+    }
+
+    private void UpdateCharacter(int SelectedCharacterP1) {
+        Player1Character = CharacterDB.GetCharacter(SelectedCharacterP1);
+    }
+
+    public void InstantiatePlayer() {
+        Vector3 PlayerSpawn = new Vector3(-5.93f, -3.277f, 0);
+
+        Player1Instance = Instantiate(Player1Character.CharacterPrefab, PlayerSpawn, Quaternion.identity);
+        Player1Animator = Player1Instance.GetComponent<Animator>();
+    }
+
+    public void InstantiateEnemy() {
+        Vector3 EnemySpawn = new Vector3(5.93f, -3.277f, 0);
+
+        Player2Instance = Instantiate(Player2Character.CharacterPrefab, EnemySpawn, Quaternion.Euler(0, 180, 0));
+        Player2Animator = Player2Instance.GetComponent<Animator>();
+    }
+}
---
+++ Assets/Scripts/Menu/Menu.cs
@@ -0,0 +1,100 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour {
+    [SerializeField] private GameObject MainMenu;
+    [SerializeField] private GameObject Settings;
+    [SerializeField] private GameObject Modes;
+    [SerializeField] private GameObject Keys;
+    [SerializeField] private GameObject Acessibility;
+
+    [SerializeField] private GameObject MainMenuButton;
+    [SerializeField] private GameObject SettingsButton;
+    [SerializeField] private GameObject ModesButton;
+    [SerializeField] private GameObject KeysButton;
+    [SerializeField] private GameObject AcessibilityButton;
+
+    public void OpenModes() {
+        MainMenu.SetActive(false);
+        Modes.SetActive(true);
+
+        EventSystem.current.SetSelectedGameObject(null);
+        EventSystem.current.SetSelectedGameObject(ModesButton);
+    }
+
+    public void CloseModes() {
+        MainMenu.SetActive(true);
+        Modes.SetActive(false);
+
+        EventSystem.current.SetSelectedGameObject(null);
+        EventSystem.current.SetSelectedGameObject(MainMenuButton);
+    }
+
+    public void OpenSettings() {
+        MainMenu.SetActive(false);
+        Settings.SetActive(true);
+
+        EventSystem.current.SetSelectedGameObject(null);
+        EventSystem.current.SetSelectedGameObject(SettingsButton);
+    }
+
+    public void CloseSettings() {
+        MainMenu.SetActive(true);
+        Settings.SetActive(false);
+
+        EventSystem.current.SetSelectedGameObject(null);
+        EventSystem.current.SetSelectedGameObject(MainMenuButton);
+    }
+
+    public void OpenKeys() {
+        Settings.SetActive(false); 
+        Keys.SetActive(true);
+
+        EventSystem.current.SetSelectedGameObject(null);
+        EventSystem.current.SetSelectedGameObject(KeysButton);
+    }
+
+    public void CloseKeys() {
+        Settings.SetActive(true); 
+        Keys.SetActive(false);
+
+        EventSystem.current.SetSelectedGameObject(null);
+        EventSystem.current.SetSelectedGameObject(SettingsButton);
+    }
+
+    public void OpenAcessibility() {
+        Settings.SetActive(false); 
+        Acessibility.SetActive(true);
+
+        EventSystem.current.SetSelectedGameObject(null);
+        EventSystem.current.SetSelectedGameObject(AcessibilityButton);
+    }
+
+    public void CloseAcessibility() {
+        Settings.SetActive(true); 
+        Acessibility.SetActive(false);
+
+        EventSystem.current.SetSelectedGameObject(null);
+        EventSystem.current.SetSelectedGameObject(SettingsButton);
+    }
+
+    public void CloseGame() {
+        Debug.Log("Saiu");
+        Application.Quit();
+    }
+
+    public void Infinite() {
+        SceneManager.LoadScene("Infinite");
+    }
+
+    public void Onevsone() {
+        SceneManager.LoadScene("1vs1");
+    }
+
+    public void Blind() {
+        SceneManager.LoadScene("Blind");
+    }
+}
